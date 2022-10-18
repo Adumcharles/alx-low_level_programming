7d6518ebfcc1269f7b61d02897660883ef1c375a
Charles Ahamefula Adum <113665051+Adumcharles@users.noreply.github.com>
2022-10-18 19:16:38 +0100

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,7 +10,7 @@ int main(void)
 	int count;
 	unsigned long a = 0, b = 1, sum;
 
-	for (count = 0, count < 50, count++)
+	for (count = 0; count < 50; count++)
 	{
 		sum = a + b;
 		printf("%lu", sum);
