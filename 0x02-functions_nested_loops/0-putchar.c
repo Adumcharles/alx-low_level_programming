a068505a3564ab76809b8c98017410bd465e5b01
Adumcharles <adumcharles10@gmail.com>
2022-10-18 03:22:09 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Print _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	return (0);
+}
