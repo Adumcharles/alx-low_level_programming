faaed81b1c2592160e089456f3fa6ce34b391a23
Adumcharles <adumcharles10@gmail.com>
2022-10-20 04:07:10 -0700

    first comnit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a >= 0; a <= 10; a++)
+	{
+		for (b >= 0; b <= 14; b++)
+		{
+			_putchar(b);
+		}
+		a++;
+	}
+	_putchar('\n');
+	return (0);
+}
