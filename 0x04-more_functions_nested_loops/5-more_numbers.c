5a027d5dc49c296f0b079b390979eb1dfb844447
Adumcharles <adumcharles10@gmail.com>
2022-10-20 05:17:42 -0700

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,10 @@ void more_numbers(void)
 	{
 		for (b = 0; b <= 14; b++)
 		{
-			_putchar(b);
+			if (j >= 10)
+				_putchar('1');
+			_putchar(j % 10 + '0');
 		}
-		a++;
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
