81b4a437121b8ba4095522782690c6226d93c5c4
Charles Ahamefula Adum <113665051+Adumcharles@users.noreply.github.com>
2022-10-20 12:35:46 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,20 +3,19 @@
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
  *
- * Return: 0
+ * Return: void
  */
 void more_numbers(void)
 {
 	int a, b;
 
-	for (a >= 0; a <= 10; a++)
+	for (a = 0; a <= 10; a++)
 	{
-		for (b >= 0; b <= 14; b++)
+		for (b = 0; b <= 14; b++)
 		{
 			_putchar(b);
 		}
 		a++;
 	}
 	_putchar('\n');
-	return (0);
 }
